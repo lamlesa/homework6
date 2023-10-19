f38649865d65963ac1f1e86f5ff6089dff122b53
morozovalesa18@gmail.com <morozovalesa18@gmail.com>
2023-10-19 23:47:25 +0300

    Добавьте файлы проекта.

---
+++ homework6/Program.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Threading;
+
+namespace homework6
+{
+    public class Drugs
+    {
+        private static bool choice = false;
+        private ushort price;
+        private byte dose;
+        public bool Choice
+        {
+            get { return choice; }
+            set { choice = value; }
+        }
+        public ushort Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+        public byte Dose
+        {
+            get { return dose; }
+            set { dose = value; }
+        }
+        public void MakeChoice()
+        {
+            Console.WriteLine("из-за тёмного угла показалась рука с перстнем и протянула вам пакетик с неопознанным содержимым, ваши действия?\n1: с локтя\n2: робко, но взять");
+            ConsoleKey key = Console.ReadKey().Key;
+            if (key == ConsoleKey.D1)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nВ кафе \"Элефант\" вошёл Штирлиц.\r\n- Это Штирлиц, сейчас будет драка, - сказал один из посетителей. Штирлиц выпил чашечку кофе и вышел.\r\n- Нет, это не Штирлиц, - возразил второй посетитель.\r\n- Нет, Штирлиц! - и тут началась драка.");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.WriteLine("\nэто действие будет иметь последствия ....");
+                choice = true;
+            }
+        }
+        public void MakeChoiceAgain()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("вы пришли домой и выбираете, что делать с наркотиками .");
+            Console.WriteLine("1: употребить\n2: выбросить");
+            ConsoleKey key = Console.ReadKey().Key;
+            if (key == ConsoleKey.D1)
+            {
+                Console.WriteLine("");
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("вы решили употребить наркотики.");
+                if (choice == true)
+                {
+                    Console.WriteLine("теперь вы Леонардо Ди Каприо в <<дневниках баскеболиста>> ....");
+                }
+                else
+                {
+                    Console.WriteLine("добрая фея дала вам подзатыльник и напомнила, что у вас ничего нет, ведь вы отказались от предложенных нарКОТИКОВ");
+                }
+            }
+            else
+            {
+                Console.WriteLine("вы решили выкинуть накротики.");
+                if (choice == true)
+                {
+                    Console.WriteLine("давай!! ДАВАААЙ УРААА ДАВААЙ ДААААВАААЙ");
+                }
+                else
+                {
+                    Console.WriteLine("вы большие умнички, ведь не поддаетесь на провокации !!");
+                }
+            }
+        }
+        public virtual void Use() 
+        {
+            Console.WriteLine("");
+            Console.WriteLine("*вы решили употребить наркотики*");
+            Console.WriteLine("вы опять убегаете от реальности .");
+        }
+    }
+    class Psychedelics : Drugs
+    {
+        public Psychedelics(ushort price)
+        {
+            Price = price;
+        }
+        public Psychedelics(ushort price, byte dose)
+        {
+            Price = price;
+            Dose = dose;
+        }
+        public void Feelings()
+        {
+            Console.WriteLine("Человек в состоянии наркотического опьянения перестает испытывать душевную и\r\nфизическую боль, появляется ощущение легкости, комфорта, происходит потеря\r\nконтроля над собой и утрата чувства реальности. ");
+            if (Choice == true)
+            {
+                Console.WriteLine("но это ненадолго ....");
+            }
+        }
+        private string[] laws = { "Статья 228", "Статья 228.1", "Статья 228.2", "Статья 228.4", "Статьи 229-233" };
+        public string[] Laws
+        {
+            get { return laws; }
+        }
+        public void PrintLaws()
+        {
+            Console.WriteLine("");
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("незаконное приобретение,\nперевозка,\nхранение,\nизготовление и переработка для употребления в личных целях веществ\nявляются нарушением следующих статей УК РФ:");
+            foreach(string law in laws)
+            {
+                Console.WriteLine(law);
+            }
+            if (Choice == false)
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.WriteLine("но вам это не грозит <3");
+            }
+            Console.ResetColor();
+        }
+    }
+    class Stimulants : Drugs
+    {
+        private static ushort overdose = 30;
+        public Stimulants(byte dose)
+        {
+            Dose = dose;
+        }
+        private static string[] consequences = { "бредовые состояния", "слуховые галлюцинации", "зрительные галлюцинации", "тремор", "нарушение сна", "аритмия"};
+        public void PrintConcequences()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("последствия употребления стимуляторов:");
+            Console.BackgroundColor = ConsoleColor.White;
+            foreach (string consequence in consequences)
+            {
+                Console.WriteLine(consequence);
+            }
+            if (Choice == false)
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.WriteLine("но вы можете об этом не беспокоиться !!");
+            }
+            Console.ResetColor();
+        }
+        public void DeathCheck()
+        {
+            Console.WriteLine();
+            Console.WriteLine("проверка на передоз...");
+            if (Dose > overdose)
+            {
+                Console.WriteLine("как называется место на кладбище, где сидит сторож ? живой уголок :))))");
+            }
+            else
+            {
+                Console.WriteLine("вы остались живы, но такими темпами... в общем тихонечко");
+            }
+        }
+    }
+    public abstract class Sedatives : Drugs
+    {
+        public override void Use()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("вы решили употребить седативное вещество .");
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("К седативным (т.е. успокаивающим) наркотикам относят опиатные наркотики и снотворные барбитуратной группы .");
+            Thread.Sleep(5000);
+            Console.ResetColor();
+        }
+    }
+    public class Opioids : Sedatives
+    {
+        public Opioids(byte dose)
+        {
+            Dose = dose;
+        }
+        public Opioids(byte dose, ushort price)
+        {
+            Dose = dose;
+            Price = price;
+        }
+        private string[] composition = { "бензольное кольцо", "азот", "этильный или пропильный «мостик»" };
+        public string[] Composition
+        {
+            get { return composition; }
+        }
+        public void ExplainComposition()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("преимущетвенный химический состав седативов:");
+            foreach(string component in Composition)
+            {
+                Console.WriteLine(component);
+            }
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Psychedelics LSD = new Psychedelics(1500, 15);
+            Psychedelics ketamine = new Psychedelics(5320);
+            Opioids heroin = new Opioids(10);
+            LSD.MakeChoice();
+            bool flag = false;
+            byte dose = 0;
+            byte k = 0;
+            do
+            {
+                if (k != 0)
+                {
+                    Console.WriteLine("а употребление сказывается на вашей мозговой деятельности ....");
+                }
+                Console.Write("введите дозу амфетамина: ");
+                flag = byte.TryParse(Console.ReadLine(), out dose);
+                k++;
+            } while (!flag);
+            Stimulants amphetamine = new Stimulants(dose);
+            amphetamine.DeathCheck();
+            amphetamine.PrintConcequences();
+            heroin.Use();
+            heroin.ExplainComposition();
+            ketamine.Use();
+            ketamine.PrintLaws();
+        }
+    }
+}
---
+++ lab7/Program.cs
@@ -0,0 +1,232 @@
+﻿using System;
+
+namespace lab7
+{
+    public enum AccountTypes
+    {
+        Current,
+        Savings
+    }
+    public class Building
+    {
+        private static ushort number = CreateBuildingNumber();
+        private double height;
+        private byte storeys;
+        private uint apartments;
+        private byte entrances;
+        static private ushort CreateBuildingNumber()
+        {
+            Random n = new Random(0);
+            bool flag = false;
+            do
+            {
+                flag = ushort.TryParse(Convert.ToString(n.Next()), out number);
+            } while (!flag);
+            return (number);
+        }
+        public void CreateNewBuildingNumber()
+        {
+            Building.number += 1;
+        }
+        public ushort Number
+        {
+            get { return number; }
+            set { number = value; }
+        }
+        public double Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+        public byte Storeys
+        {
+            get { return storeys; }
+            set { storeys = value; }
+        }
+        public uint Apartments
+        {
+            get { return apartments; }
+            set { apartments = value; }
+        }
+        public byte Entrances
+        {
+            get { return entrances  ; }
+            set { entrances = value; }
+        }
+        public void CalculateTheHeightOfTheStorey(double height, byte storeys)
+        {
+            double storey_height = height / Convert.ToDouble(storeys);
+            Console.WriteLine($"Высота одного этажа: {storey_height}");
+        }
+        public void CalculateTheNumberOfApartmentsInTheEntrance(uint apartments, byte entrances)
+        {
+            double entrance_volume = Convert.ToDouble(apartments) / Convert.ToDouble(entrances);
+            if (entrance_volume % 1 == 0)
+            {
+                Console.WriteLine($"Квартир в одном подъезде: {entrance_volume}");
+            }
+            else
+            {
+                throw new ArgumentException("Что-то не так с входными данными.");
+            }            
+        }
+        public void CalculateTheNumberOfApartmentsPerStorey(uint apartments, byte storeys)
+        {
+            double storey_volume = Convert.ToDouble(apartments) / Convert.ToDouble(storeys);
+            if (storey_volume % 1 == 0)
+            {
+                Console.WriteLine($"Квартир на одном этаже: {storey_volume}");
+            }
+            else
+            {
+                throw new ArgumentException("Что-то не так с входными данными.");
+            }            
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Номер здания: {number}\nВысота: {height}\nКоличество квартир: {apartments}\nПодъездов: {entrances}\nЭтажей: {storeys}");
+        }
+    }
+    public class BankAccount
+    {
+        static private ulong number = CreateAccountNumber();
+        private double balance;
+        private AccountTypes type;
+        public ulong Number
+        {
+            get { return number; }
+            set { number = value; }
+        }
+        public double Balance
+        {
+            get { return balance; }
+            set { balance = value; }
+        }
+        public AccountTypes Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+        static private ulong CreateAccountNumber()
+        {
+            Random n = new Random(0);
+            bool flag = false;
+            do
+            {
+                flag = ulong.TryParse(Convert.ToString(n.Next()), out number);
+            } while (!flag);
+            return (number);
+        }
+        public void CreateNewAccountNumber()
+        {
+            BankAccount.number += 1;
+        }
+        public void WithdrawMoney(double sum)
+        {
+            if (sum > balance)
+            {
+                throw new ArgumentOutOfRangeException("У вас нет таких денег.");
+            }
+            else
+            {
+                balance -= sum;
+            }
+        }
+        public void PutMoney(double sum)
+        {
+            balance += sum;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Номер счёта: {number}\nБаланс: {balance}\nТип счёта: {type}");
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Лабораторная 7:");
+            Console.WriteLine("( после каждого ввода стоит нажимать Enter )");
+            double balance = 0, sum = 0;
+            bool flag = false;
+            byte type = 0;
+            BankAccount account = new BankAccount();
+            account.CreateNewAccountNumber();
+            do
+            {
+                Console.Write("Сколько денег находится на счету? ");
+                flag = double.TryParse(Console.ReadLine(), out balance);
+            } while (!flag);
+            account.Balance = balance;
+            Console.WriteLine("( 1 - текущий, 2 - сберегательный )");
+            do
+            {
+                Console.Write("Какой тип счёта? ");
+                flag = byte.TryParse(Console.ReadLine(), out type);
+                if ((type != 1) && (type != 2))
+                {
+                    flag = false;
+                }
+            } while (!flag);
+            if (type == 1)
+            {
+                account.Type = AccountTypes.Current;
+            }
+            else
+            {
+                account.Type = AccountTypes.Savings;
+            }
+            do
+            {
+                Console.Write("Сколько денег вы бы хотели снять со счёта? ");
+                flag = double.TryParse(Console.ReadLine(), out sum);
+            } while (!flag);
+            try
+            {
+                account.WithdrawMoney(sum);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+            do
+            {
+                Console.Write("Сколько денег вы бы хотели положить на счёт? ");
+                flag = double.TryParse(Console.ReadLine(), out sum);
+            } while (!flag);
+            account.PutMoney(sum);
+            account.Print();
+
+            Building building1 = new Building();
+            building1.CreateNewBuildingNumber();
+            building1.Height = 90.74;
+            building1.Storeys = 5;
+            building1.Apartments = 75;
+            building1.Entrances = 4;
+            building1.Print();
+            building1.CalculateTheHeightOfTheStorey(building1.Height, building1.Storeys);
+            try
+            {
+                building1.CalculateTheNumberOfApartmentsInTheEntrance(building1.Apartments, building1.Entrances);
+            }
+            catch(ArgumentException ex)
+            {
+                Console.WriteLine($"(в подъезде) Ошибка: {ex.Message}");
+            }
+            try
+            {
+                building1.CalculateTheNumberOfApartmentsPerStorey(building1.Apartments, building1.Storeys);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"(на этаже) Ошибка: {ex.Message}");
+            }
+
+
+            // посмотреть изменения уникального номера .
+            Building building2 = new Building();
+            building2.CreateNewBuildingNumber();
+            building2.Print();
+        }
+    }
+}
